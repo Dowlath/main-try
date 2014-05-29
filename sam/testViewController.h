eb739e78a34dcfef16e954d8f2db6fc765cecb7b
Dowlath <dowlath.s@hispark.in>
2014-05-29 16:46:01 +0530

    assessment own
    own

---
+++ sam/testViewController.h
@@ -0,0 +1,23 @@
+//
+//  testViewController.h
+//  sam
+//
+//  Created by BSA Univ3 on 27/05/14.
+//  Copyright (c) 2014 BSA Univ3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+@interface testViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIView *pop;
+@property AppDelegate *appdelegate;
+- (IBAction)next:(id)sender;
+- (IBAction)SAVE:(id)sender;
+- (IBAction)fetch:(id)sender;
+@property (strong, nonatomic) IBOutlet UITextField *name;
+@property (strong, nonatomic) IBOutlet UITextField *address;
+@property (strong, nonatomic) IBOutlet UITextField *mob;
+-(NSArray *)fetches;
+
+@end
